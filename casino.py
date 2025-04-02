88b12e1fa2dd6665cacdc880ded0e24e7e551972
Dany8239 <dany.koch.9@gmail.com>
2025-04-02 10:58:28 +0200

    First commit

---
+++ casino.py
@@ -0,0 +1,24 @@
+import random as r
+punkte = 5
+print("Vítej v casinu")
+while True:
+    try:
+        range = int(input("Jakou chceš mít horní hranici pro náhodné číslo? Čím vyšší rozdíl, tím vyšší odměna: "))
+        guess = int(input(f"Typni si číslo od 1 do {range}: "))
+        number = r.randint(1, range)
+        reward = range-1
+
+        if guess == number:
+            print("Gratulujeme, vyhrál jsi magický bodík")
+            punkte += reward
+        else:
+            print("Bohužel jsi prohrál/a")
+            punkte -= 1
+        print("Máš", punkte, "bodíků")
+        if punkte <= 0:
+            print("Došly ti peníze, bohužel končíš")
+            break
+    except ValueError:
+        print("Zadaná hodnota není číselná")
+        continue
+
